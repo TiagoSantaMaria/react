ed458deb9cf38032217b1605dc29db8146f933ea
Tiago Santa Maria <tiagosantamaria01@gmail.com>
2022-08-20 01:46:24 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import CartWidget from './components/CartWidget/CartWidget';
+import NavBar from './components/Navbar/NavBar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <CartWidget
+      name='Hamburguesa Triple'
+      img='https://www.clarin.com/img/2021/05/20/Lj4vcOHr__720x0__1.jpg'
+      desc='Triple Carne, Panceta y CHEDDAR'
+      />
     </div>
   );
 }
---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function CartWidget({name, img, desc}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height=""
+          image={img}
+          alt="hamburguesa"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {desc}  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
